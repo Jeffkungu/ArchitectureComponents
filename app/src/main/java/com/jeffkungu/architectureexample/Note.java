b9d50d5b17ed7add1ea73a14bb8bd59ec1dce9e0
Jeff Kungu Gitenguri <jgitenguri@safaricom.co.ke>
2019-01-07 10:41:15 +0300

    [Feature] init project

---
+++ app/src/main/java/com/jeffkungu/architectureexample/Note.java
@@ -0,0 +1,11 @@
+package com.jeffkungu.architectureexample;
+
+import android.arch.persistence.room.Entity;
+
+/**
+ * Created by jeffkungu on 05/12/2018.
+ */
+
+@Entity(tableName = "note_table")
+public class Note {
+}
